8606871e5dbdddc874c5981fb8df2399d859ffe4
Leticia Forte <leticilialety@gmail.com>
2026-06-09 22:27:00 -0300

    all working

--- avaliacao_4/ms_notificacao.py
+++ avaliacao_4/ms_notificacao.py
@@ -4,21 +4,21 @@ import pika
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
-#import resend
+import resend
 
-#with open(".env") as f:
- #   for linha in f:
-  #      linha = linha.strip()
+with open(".env") as f:
+    for linha in f:
+        linha = linha.strip()
 
-   #     if not linha or linha.startswith("#"):
-    #        continue
+        if not linha or linha.startswith("#"):
+            continue
 
-      #  chave, valor = linha.split("=", 1)
-     #   os.environ[chave.strip()] = valor.strip()
+        chave, valor = linha.split("=", 1)
+        os.environ[chave.strip()] = valor.strip()
 
-#print(os.environ["RESEND_API_KEY"])
+print(os.environ["RESEND_API_KEY"])
 
-#resend.api_key = os.environ["RESEND_API_KEY"]
+resend.api_key = os.environ["RESEND_API_KEY"]
 
 if os.path.exists("./public_keys/mspromocao_publickey.pem"):
     with open("./public_keys/mspromocao_publickey.pem", "rb") as f:
@@ -37,7 +37,6 @@ else:
     exit(1)
 
 with open('promocao_categorias.txt', 'r', encoding='utf-8') as f:
-    #lista_categorias = f.readlines()
     lista_categorias = [linha.strip() for linha in f.readlines() if linha.strip()]
 
 def callback(ch, method, properties, body):
@@ -85,20 +84,20 @@ def callback(ch, method, properties, body):
                     promocao["title"] = f"HOT DEAL: {promocao['title']}"
                 print("HOT DEAL recebido!")
                 print(promocao['title'])
-               # resend.Emails.send({
-               # "from": "onboarding@resend.dev",
-                #"to": "pedro.fallgatter@gmail.com",
-                #"subject": f"Sua promoção {promocao['title']} entrou em destaque!",
-                #"html": "<p>Parabéns! Sua promoção se tornou um HOT DEAL!s.</p>"
-                #})
-                #print("Email de HOT DEAL enviado via Resend.")
+                resend.Emails.send({
+                "from": "onboarding@resend.dev",
+                "to": "leticiaforte@alunos.utfpr.edu.br",
+                "subject": f"Sua promoção {promocao['title']} entrou em destaque!",
+                "html": "<p>Parabéns! Sua promoção se tornou um HOT DEAL!s.</p>"
+                })
+                print("Email de HOT DEAL enviado via Resend.")
             else:
-                #resend.Emails.send({
-                #"from": "onboarding@resend.dev",
-                #"to": "pedro.fallgatter@gmail.com",
-                #"subject": f"Sua promoção {promocao['title']} foi aprovada!",
-                #"html": "<p>Sua promoção foi aprovada e está disponível para visualização.</p>"
-                #})
+                resend.Emails.send({
+                "from": "onboarding@resend.dev",
+                "to": "leticiaforte@alunos.utfpr.edu.br",
+                "subject": f"Sua promoção {promocao['title']} foi aprovada!",
+                "html": "<p>Sua promoção foi aprovada e está disponível para visualização.</p>"
+                })
                 print("Email de promoção aprovada enviado via Resend.")
 
             new_body = json.dumps(promocao)
--- avaliacao_4/run_all.bat
+++ avaliacao_4/run_all.bat
@@ -4,3 +4,4 @@ cmd /c start "Gateway" cmd /k python ms_gateway.py
 cmd /c start "Notificacao" cmd /k python ms_notificacao.py
 cmd /c start "Promocao" cmd /k python ms_promocao.py
 cmd /c start "Ranking" cmd /k python ms_ranking.py
+cmd /c start "Frontend" cmd /k python -m http.server 5500
